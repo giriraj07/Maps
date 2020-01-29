ac779c7662b9fc541de0c6861238c533ee310fa1
Giriraj Kishor <girirajrpvv@gmail.com>
2020-01-29 12:49:41 +0530

    Commit files

--- AlfredDemo/app/src/main/java/com/example/alfreddemo/MainActivity.java
+++ AlfredDemo/app/src/main/java/com/example/alfreddemo/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.alfreddemo;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CheckedTextView;
 import android.widget.GridView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -10,7 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         GridView gv1 = findViewById(R.id.gridView);
-        gv1.setAdapter(new LangAdapter(this));
+        final TextView tv=findViewById(R.id.tv);
+        gv1.setAdapter(new LangAdapter(getBaseContext()));
+        gv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                CheckedTextView textView= (CheckedTextView) parent.getItemAtPosition(position);
+                if(textView!=null) {
+                    textView.setCheckMarkDrawable(R.drawable.green);
+                    tv.setVisibility(View.VISIBLE);
+                }
+            }
+      });
     }
 }
 
